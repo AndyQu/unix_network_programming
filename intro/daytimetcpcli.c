1a9d9480e5a1ca9bc087ca22467c7faabf8d1207
root <root@andyqu-dev.office.mos>
2017-04-11 06:00:30 -0400

    add comments

--- intro/daytimetcpcli.c
+++ intro/daytimetcpcli.c
@@ -1,5 +1,17 @@
 #include    "unp.h"
 
+/*
+struct sockaddr_in
+
+functions:
+socket()
+htons()
+inet_pton()
+connect()
+read()
+fputs()
+
+*/
 int
 main(int argc, char **argv)
 {
@@ -10,15 +22,20 @@ main(int argc, char **argv)
     if (argc != 2)
         err_quit("usage: a.out <IPaddress>");
 
+    //Create a socket
+    //AF_INET: Internet
+    //SOCK_STREAM: stream type
     if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
         err_sys("socket error");
 
+    //configure a servaddr_in object: sin_family, sin_port, sin_addr
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port   = htons(13);    /* daytime server */
     if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
         err_quit("inet_pton error for %s", argv[1]);
 
+    //connect to the daytime server
     if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
         err_sys("connect error");
 
